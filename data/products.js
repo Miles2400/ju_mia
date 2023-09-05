23a25329448f91cf7359c11424a13e618d71b3d0
Miles2400 <piusjay40@gmail.com>
2023-09-05 08:26:42 +0100

    created half of the page

---
+++ data/products.js
@@ -0,0 +1,64 @@
+ const productCategories = [
+	{
+		imageSrc: "../images/phones-tablets_300x240.png",
+		categoryName: "Phones & Tablets",
+		link: ""
+	},
+	{
+		imageSrc: "../images/generic_300x240.jpg",
+		categoryName: "Computer Deals",
+		link: ""
+	},
+	{
+		imageSrc: "../images/groceries_300x240v2.png",
+		categoryName: "Groceries",
+		link: ""
+	},
+	{
+		imageSrc: "../images/5k-store_300x240.png",
+		categoryName: "#5,000 Store",
+		link: ""
+	},
+	{
+		imageSrc: "../images/televisions_300x240.png",
+		categoryName: "Televisions",
+		link: ""
+	},
+	{
+		imageSrc: "../images/refrigerators_300x240.png",
+		categoryName: "Refrigerators",
+		link: ""
+	},
+	{
+		imageSrc: "../images/earphones_300x240.png",
+		categoryName: "Mobile Accessories",
+		link: ""
+	},
+	{
+		imageSrc: "../images/generator.gif",
+		categoryName: "Generators",
+		link: ""
+	},
+	{
+		imageSrc: "../images/fashion_300x240.png",
+		categoryName: "Men's Sneakers",
+		link: ""
+	},
+	{
+		imageSrc: "../images/watches_300x240.png",
+		categoryName: "Watches",
+		link: ""
+	},
+	{
+		imageSrc: "../images/Clearance.gif",
+		categoryName: "Clearance Sales",
+		link: ""
+	},
+	{
+		imageSrc: "../images/300pxx240px.gif",
+		categoryName: "Authenticity Guaranteed",
+		link: ""
+	}
+];
+
+
