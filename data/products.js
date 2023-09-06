3c97481e85859e396ed4cb425af70835e240b675
Miles2400 <piusjay40@gmail.com>
2023-09-06 23:24:05 +0100

    update from developer

--- data/products.js
+++ data/products.js
@@ -1,64 +1,27 @@
- const productCategories = [
+const collectionsData = [
 	{
-		imageSrc: "../images/phones-tablets_300x240.png",
-		categoryName: "Phones & Tablets",
-		link: ""
+		imageSrc: "../images/phone-deals_300x400.png",
+		category: "Phone Deals"
 	},
 	{
-		imageSrc: "../images/generic_300x240.jpg",
-		categoryName: "Computer Deals",
-		link: ""
+		imageSrc: "../images/fashion-deals_300x400.png",
+		category: "Fashion Deals"
 	},
 	{
-		imageSrc: "../images/groceries_300x240v2.png",
-		categoryName: "Groceries",
-		link: ""
+		imageSrc: "../images/supermarket-deals_300x400.jpg",
+		category: "Supermarket Deals"
 	},
 	{
-		imageSrc: "../images/5k-store_300x240.png",
-		categoryName: "#5,000 Store",
-		link: ""
+		imageSrc: "../images/electronics-deals_300x400.png",
+		category: "Electronics Deals"
 	},
 	{
-		imageSrc: "../images/televisions_300x240.png",
-		categoryName: "Televisions",
-		link: ""
+		imageSrc: "../images/appliances-deals_300x400.png",
+		category: "Appliances Deals"
 	},
 	{
-		imageSrc: "../images/refrigerators_300x240.png",
-		categoryName: "Refrigerators",
-		link: ""
-	},
-	{
-		imageSrc: "../images/earphones_300x240.png",
-		categoryName: "Mobile Accessories",
-		link: ""
-	},
-	{
-		imageSrc: "../images/generator.gif",
-		categoryName: "Generators",
-		link: ""
-	},
-	{
-		imageSrc: "../images/fashion_300x240.png",
-		categoryName: "Men's Sneakers",
-		link: ""
-	},
-	{
-		imageSrc: "../images/watches_300x240.png",
-		categoryName: "Watches",
-		link: ""
-	},
-	{
-		imageSrc: "../images/Clearance.gif",
-		categoryName: "Clearance Sales",
-		link: ""
-	},
-	{
-		imageSrc: "../images/300pxx240px.gif",
-		categoryName: "Authenticity Guaranteed",
-		link: ""
+		imageSrc: "../images/beauty-deals_300x400v2.png",
+		category: "Beauty Deals"
 	}
 ];
-
 
